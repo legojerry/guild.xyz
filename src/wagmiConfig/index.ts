8c9f9af92dde26a625f0c732e00831ca3e2ae69b
legojerry <38884404+legojerry@users.noreply.github.com>
2024-04-24 23:24:48 -0700

    if you throw enough shit at the wall, something will stick~ abraham lincoln, probably

--- src/wagmiConfig/index.ts
+++ src/wagmiConfig/index.ts
@@ -1,274 +1,194 @@
-import { PublicUser } from "components/[guild]/hooks/useUser"
-import {
-  beraTestnet,
-  bitfinityTestnet,
-  exosama,
-  formTestnet,
-  metisSepolia,
-  neonEVM,
-  oasisSapphire,
-  ontology,
-  palm,
-  taikoKatlaTestnet,
-  x1,
-} from "static/customChains"
-import { mutate } from "swr"
-import fetcher, { fetcherWithSign } from "utils/fetcher"
-import { http, type Chain, type WalletClient } from "viem"
-import { mnemonicToAccount } from "viem/accounts"
-import { createConfig, type Connector } from "wagmi"
-import {
-  arbitrum,
-  arbitrumNova,
-  astarZkEVM,
-  avalanche,
-  base,
-  baseSepolia,
-  blast,
-  blastSepolia,
-  boba,
-  bsc,
-  celo,
-  coreDao,
-  cronos,
-  evmos,
-  fantom,
-  gnosis,
-  harmonyOne,
-  kava,
-  linea,
-  liskSepolia,
-  lukso,
-  mainnet,
-  manta,
-  mantle,
-  metis,
-  moonbeam,
-  moonriver,
-  opBNB,
-  optimism,
-  pgn,
-  polygon,
-  polygonMumbai,
-  polygonZkEvm,
-  ronin,
-  scroll,
-  scrollSepolia,
-  sepolia,
-  shimmer,
-  x1Testnet,
-  zetachain,
-  zetachainAthensTestnet,
-  zkSync,
-  zora,
-} from "wagmi/chains"
-import { coinbaseWallet, injected, safe, walletConnect } from "wagmi/connectors"
-import { mock } from "wagmiConfig/mockConnector"
-import waasConnector from "wagmiConfig/waasConnector"
-
 /**
- * We should consider adding only those chains here which we actually use for
- * client-side interactions (e.g. where users can mint Guild Pins, NFTs, buy tokens,
- * etc.)
+ * We couldn't use the `mock` connector for testing real interaction in our E2E
+ * tests, so we copy-pasted its source code here, extended it:
+ *
+ * - Passing Account[] in params instead of a `0x${string}`[]
+ * - Implemented the `getClient` method
+ * - Added supportfor the `eth_accounts` method inside the `getProvider` function
  */
-export const wagmiConfig = createConfig({
-  chains: [
-    mainnet,
-    polygon,
-    polygonMumbai,
-    polygonZkEvm,
-    base as Chain,
-    baseSepolia as Chain,
-    optimism as Chain,
-    arbitrum,
-    arbitrumNova,
-    bsc,
-    avalanche,
-    gnosis,
-    fantom,
-    celo as Chain,
-    harmonyOne,
-    moonbeam,
-    moonriver,
-    metis,
-    metisSepolia,
-    cronos,
-    boba,
-    palm,
-    exosama,
-    evmos,
-    zetachain,
-    zetachainAthensTestnet,
-    scroll,
-    scrollSepolia,
-    zkSync as Chain,
-    zora as Chain,
-    pgn,
-    neonEVM,
-    linea,
-    lukso,
-    mantle,
-    ronin,
-    shimmer,
-    kava,
-    bitfinityTestnet,
-    x1,
-    x1Testnet,
-    ontology,
-    beraTestnet,
-    manta,
-    taikoKatlaTestnet,
-    blast,
-    blastSepolia,
-    oasisSapphire,
-    sepolia,
-    astarZkEVM,
-    coreDao,
-    liskSepolia as Chain,
-    opBNB,
-    formTestnet,
-  ],
-  transports: {
-    [mainnet.id]: http(),
-    [polygon.id]: http("https://polygon-bor-rpc.publicnode.com"),
-    [polygonMumbai.id]: http(),
-    [polygonZkEvm.id]: http(),
-    [base.id]: http("https://base-rpc.publicnode.com"),
-    [baseSepolia.id]: http(),
-    [optimism.id]: http(),
-    [arbitrum.id]: http(),
-    [arbitrumNova.id]: http(),
-    [bsc.id]: http(),
-    [avalanche.id]: http(),
-    [gnosis.id]: http(),
-    [fantom.id]: http(),
-    [celo.id]: http(),
-    [harmonyOne.id]: http(),
-    [moonbeam.id]: http(),
-    [moonriver.id]: http(),
-    [metis.id]: http(),
-    [metisSepolia.id]: http(),
-    [cronos.id]: http(),
-    [boba.id]: http(),
-    [palm.id]: http(),
-    [exosama.id]: http(),
-    [evmos.id]: http("https://evmos.lava.build"),
-    [zetachain.id]: http(),
-    [zetachainAthensTestnet.id]: http(),
-    [scroll.id]: http(),
-    [scrollSepolia.id]: http(),
-    [zkSync.id]: http(),
-    [zora.id]: http(),
-    [pgn.id]: http(),
-    [neonEVM.id]: http(),
-    [linea.id]: http(),
-    [lukso.id]: http(),
-    [mantle.id]: http(),
-    [ronin.id]: http(),
-    [shimmer.id]: http(),
-    [kava.id]: http(),
-    [bitfinityTestnet.id]: http(),
-    [x1.id]: http(),
-    [x1Testnet.id]: http(),
-    [ontology.id]: http(),
-    [beraTestnet.id]: http(),
-    [manta.id]: http(),
-    [taikoKatlaTestnet.id]: http(),
-    [blast.id]: http(),
-    [blastSepolia.id]: http(),
-    [oasisSapphire.id]: http(),
-    [sepolia.id]: http(),
-    [astarZkEVM.id]: http(),
-    [coreDao.id]: http(),
-    [liskSepolia.id]: http(),
-    [opBNB.id]: http(),
-    [formTestnet.id]: http(),
-  },
-  ssr: true,
-  connectors: process.env.NEXT_PUBLIC_MOCK_CONNECTOR
-    ? [
-        mock({
-          accounts: [mnemonicToAccount(process.env.NEXT_PUBLIC_E2E_WALLET_MNEMONIC)],
-        }),
-      ]
-    : [
-        injected(),
-        coinbaseWallet({
-          appName: "Guild.xyz",
-        }),
-        walletConnect({
-          projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-          showQrModal: true,
-          qrModalOptions: {
-            explorerRecommendedWalletIds: [
-              "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709", // OKX
-              "107bb20463699c4e614d3a2fb7b961e66f48774cb8f6d6c1aee789853280972c", // Bitcoin.com
-              "541d5dcd4ede02f3afaf75bf8e3e4c4f1fb09edb5fa6c4377ebf31c2785d9adf", // Ronin
-              "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", // Trust
-            ],
-            themeVariables: {
-              "--wcm-z-index": "10001",
-              "--w3m-z-index": "10001",
-            } as any, // casting it, so `--wcm-z-index` is accepted
-          },
-        }),
-        safe({
-          allowedDomains: [/gnosis-safe\.io$/, /app\.safe\.global$/],
-          debug: false,
-        }),
-        waasConnector({
-          provideAuthToken: async () => {
-            const connector = wagmiConfig.connectors.find(
-              (conn) => conn.id === 'foo'
-            ) as Connector
 
-            const walletClient = await connector
-              .getClient()
-              .catch(() => null as WalletClient)
+import {
+  Account,
+  RpcRequestError,
+  SwitchChainError,
+  UserRejectedRequestError,
+  createWalletClient,
+  custom,
+  fromHex,
+  getAddress,
+  http,
+  numberToHex,
+  type Address,
+  type Client,
+  type EIP1193RequestFn,
+  type Hex,
+  type Transport,
+  type WalletRpcSchema,
+} from "viem"
+import { rpc } from "viem/utils"
+import { ChainNotConfiguredError, createConnector, normalizeChainId } from "wagmi"
+import { type MockParameters } from "wagmi/connectors"
 
-            // First we check if there is data in the cache
-            const userCheck = !!walletClient
-              ? await mutate<PublicUser>(
-                  `/v2/users/${walletClient.account.address.toLowerCase()}/profile`,
-                  (prev) => prev,
-                  { revalidate: false }
-                )
-              : null
+mock.type = "mock" as const
+export function mock(
+  parameters: Omit<MockParameters, "accounts"> & {
+    accounts: readonly [Account, ...Account[]]
+  }
+) {
+  const features = parameters.features ?? {}
 
-            // If there isn't, we mutate
-            const user = !!walletClient
-              ? !userCheck
-                ? await mutate<PublicUser>(
-                    `/v2/users/${walletClient.account.address.toLowerCase()}/profile`
-                  )
-                : userCheck
-              : null
+  type Provider = ReturnType<
+    Transport<"custom", NonNullable<unknown>, EIP1193RequestFn<WalletRpcSchema>>
+  >
+  let connected = false
+  let connectedChainId: number
 
-            const shouldSign =
-              walletClient &&
-              walletClient?.account?.address &&
-              user?.keyPair?.keyPair
+  return createConnector<Provider>((config) => ({
+    id: "mock",
+    name: "Mock Connector",
+    type: mock.type,
+    async setup() {
+      connectedChainId = config.chains[0].id
+    },
+    async connect({ chainId } = {}) {
+      if (features.connectError) {
+        if (typeof features.connectError === "boolean")
+          throw new UserRejectedRequestError(new Error("Failed to connect."))
+        throw features.connectError
+      }
 
-            const token = shouldSign
-              ? await fetcherWithSign(
-                  {
-                    address: walletClient?.account?.address,
-                    publicClient: null,
-                    walletClient,
-                    keyPair: user?.keyPair?.keyPair,
-                  },
-                  "/v2/third-party/coinbase/token",
-                  { method: "GET" }
-                )
-              : await fetcher("/v2/third-party/coinbase/token")
+      const provider = await this.getProvider()
+      const accounts = await provider.request({
+        method: "eth_requestAccounts",
+      })
 
-            return token
-          },
-          collectAndReportMetrics: true,
-          prod:
-            (typeof window !== "undefined" &&
-              window.origin === "https://guild.xyz") ||
-            undefined,
-        }),
-      ],
-})
+      let currentChainId = await this.getChainId()
+      if (chainId && currentChainId !== chainId) {
+        const chain = await this.switchChain({ chainId })
+        currentChainId = chain.id
+      }
+
+      connected = true
+
+      return { accounts, chainId: currentChainId }
+    },
+    async disconnect() {
+      connected = false
+    },
+    async getAccounts() {
+      if (!connected) throw new Error("Connector is not connected")
+      const provider = await this.getProvider()
+      const accounts = await provider.request({ method: "eth_accounts" })
+      return accounts.map((x) => getAddress(x))
+    },
+    async getChainId() {
+      const provider = await this.getProvider()
+      const hexChainId = await provider.request({ method: "eth_chainId" })
+      return fromHex(hexChainId, "number")
+    },
+    async isAuthorized() {
+      if (!features.reconnect) return false
+      if (!connected) return false
+      const accounts = await this.getAccounts()
+      return !!accounts.length
+    },
+    async switchChain({ chainId }) {
+      const provider = await this.getProvider()
+      const chain = config.chains.find((x) => x.id === chainId)
+      if (!chain) throw new SwitchChainError(new ChainNotConfiguredError())
+
+      await provider.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: numberToHex(chainId) }],
+      })
+      return chain
+    },
+    onAccountsChanged(accounts) {
+      if (accounts.length === 0) this.onDisconnect()
+      else
+        config.emitter.emit("change", {
+          accounts: accounts.map((x) => getAddress(x)),
+        })
+    },
+    onChainChanged(chain) {
+      const chainId = normalizeChainId(chain)
+      config.emitter.emit("change", { chainId })
+    },
+    async onDisconnect() {
+      config.emitter.emit("disconnect")
+      connected = false
+    },
+    async getProvider({ chainId } = {}) {
+      const chain = config.chains.find((x) => x.id === chainId) ?? config.chains[0]
+
+      const url = chain.rpcUrls.default.http[0]
+
+      const request: EIP1193RequestFn = async ({ method, params }) => {
+        // eth methods
+        if (method === "eth_chainId") return numberToHex(connectedChainId)
+        if (method === "eth_requestAccounts")
+          return parameters.accounts.map((acc) => acc.address)
+        if (method === "eth_signTypedData_v4")
+          if (features.signTypedDataError) {
+            if (typeof features.signTypedDataError === "boolean")
+              throw new UserRejectedRequestError(
+                new Error("Failed to sign typed data.")
+              )
+            throw features.signTypedDataError
+          }
+
+        if (method === "eth_accounts")
+          return parameters.accounts.map((acc) => acc.address)
+
+        // wallet methods
+        if (method === "wallet_switchEthereumChain") {
+          if (features.switchChainError) {
+            if (typeof features.switchChainError === "boolean")
+              throw new UserRejectedRequestError(
+                new Error("Failed to switch chain.")
+              )
+            throw features.switchChainError
+          }
+          type Params = [{ chainId: Hex }]
+          connectedChainId = fromHex((params as Params)[0].chainId, "number")
+          this.onChainChanged(connectedChainId.toString())
+          return
+        }
+
+        // other methods
+        if (method === "personal_sign") {
+          if (features.signMessageError) {
+            if (typeof features.signMessageError === "boolean")
+              throw new UserRejectedRequestError(
+                new Error("Failed to sign message.")
+              )
+            throw features.signMessageError
+          }
+          // Change `personal_sign` to `eth_sign` and swap params
+          method = "eth_sign"
+          type Params = [data: Hex, address: Address]
+          params = [(params as Params)[1], (params as Params)[0]]
+        }
+
+        const body = { method, params }
+
+        const { error, result } = await rpc.http(url, { body })
+        if (error) throw new RpcRequestError({ body, error, url })
+
+        return result
+      }
+
+      return custom({ request })({ retryCount: 0 })
+    },
+    async getClient({ chainId }) {
+      const client = createWalletClient({
+        transport: http(
+          config.chains.find((c) => c.id === chainId).rpcUrls.default.http[0]
+        ),
+        account: parameters.accounts[0],
+      }) as Client
+
+      return client
+    },
+  }))
+}
